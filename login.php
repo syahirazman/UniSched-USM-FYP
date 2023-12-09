6d44e16e79dc84548e94c93fec0ba213e142b954
syahirazman <syahirahnorazman@gmail.com>
2023-12-09 22:44:12 +0800

    added and started admin dashboard

--- login.php
+++ login.php
@@ -63,8 +63,11 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="mainstyle.css">
+        <!-- styles -->
+        <link rel="stylesheet" href="css/mainstyle.css">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
