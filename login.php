3782aee0754143af4328c0fb4e6f708f945feee9
syahirazman <syahirahnorazman@gmail.com>
2023-12-10 00:37:59 +0800

    added sidebar and topbar for admin dashboard

--- login.php
+++ login.php
@@ -63,11 +63,10 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <!-- styles -->
-        <link rel="stylesheet" href="css/mainstyle.css">
+        <link rel="stylesheet" href="../css/mainstyle.css">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -137,7 +136,6 @@
                 }
             }
            
-        </script>
         
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.bundle.min.js"></script>
