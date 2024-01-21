f5011a55d0632f6ddb1b5e18de6b99e27f1a58af
syahirazman <syahirahnorazman@gmail.com>
2024-01-22 02:53:42 +0800

    Added admin course management page (html)

--- login.php
+++ login.php
@@ -66,9 +66,9 @@
         
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <!-- styles -->
-        <link rel="stylesheet" href="../css/mainstyle.css">
+        <link rel="stylesheet" href="./css/mainstyle.css">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
     
